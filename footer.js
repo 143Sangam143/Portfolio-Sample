56ca9403b25f3ae963ba827f3a030201c1f032ac
143Sangam143 <katwalsangam@gmai.com>
2023-12-05 11:14:56 +0545

    Portfolio Sample Design

---
+++ footer.js
@@ -0,0 +1,14 @@
+document.write('\
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>\
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>\
+\
+\
+	<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>\
+\
+\
+	<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>\
+\
+\
+	<script type="text/javascript" src="script.js"></script>\
+\
+');
---
+++ header.js
@@ -0,0 +1,15 @@
+document.write('\
+\
+		<!-- Swiper Link -->\
+	    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>\
+\
+	    <!-- Bootstrap Link-->\
+	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >\
+\
+	    <!-- font awesome link -->\
+	    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">\
+\
+	    <link rel="stylesheet" type="text/css" href="https://code.jquery.com/jquery-3.3.1.slim.min.js">\
+	    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">\
+\
+');
